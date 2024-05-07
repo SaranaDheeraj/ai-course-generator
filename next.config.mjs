b71e8e44b17fd5fd305938df327e341e490af71a
Likith Kumar P M <126156540+likith3105@users.noreply.github.com>
2024-05-07 19:56:55 +0530

    modified next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ["lh3.googleusercontent.com", "s3.us-west-2.amazonaws.com"],
   },
+  eslint:{
+    ignoreDuringBuilds: true,
+  }
 };
 
 export default nextConfig;
