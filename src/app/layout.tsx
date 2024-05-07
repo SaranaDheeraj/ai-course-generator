e89154503e7c570101b1e8501c6b3466c0eebe4f
Likith Kumar P M <126156540+likith3105@users.noreply.github.com>
2024-05-07 19:02:57 +0530

    modified esling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,40 +1,36 @@
-import type { Metadata } from "next";
-import { Lexend } from "next/font/google";
-import "./globals.css";
-import { cn } from "@/lib/utils";
-import Navbar from "@/components/Navbar";
-import { Provider } from "@/components/Providers";
-import { Toaster } from "@/components/ui/toaster";
-import { Providers as ChakraProvider } from "./Providers";
+import { cn } from '@/lib/utils';
+import './globals.css'
+import type { Metadata } from 'next'
+import { Lexend } from 'next/font/google'
+import Navbar from '@/components/Navbar';
+import { Provider } from '@/components/Providers';
+import { Toaster } from '@/components/ui/toaster';
+
+const lexend = Lexend({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Learn Genius",
+  title: 'Learn Genius',
+  
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="en">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Teko:wght@300..700&display=swap"
-          rel="stylesheet"
-        />
-      </head>
-      <body>
+      <body className={cn(
+        lexend.className,'antialised min-h-screen pt-16'
+      )}>
         <Provider>
-          <ChakraProvider>
-            <Navbar />
-            {children}
-            <Toaster />
-          </ChakraProvider>
+          
+        
+        <Navbar/>
+        {children}
+        <Toaster/>
         </Provider>
       </body>
     </html>
-  );
+  )
 }
